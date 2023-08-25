eacf315fee4a52bd66001b501b7e88894abc6583
mahsin-ahmed <mahsin.cse@gmail.com>
2023-08-25 06:37:36 +0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'loginLogout';
+  userName:any = '';
+  password:any = '';
+
+  Users =  {
+    userName :'mahsin',
+    password:'582596',
+  }
+
+  Signin(){
+    if(this.userName == this.Users.userName && this.password == this.Users.password){
+      console.log('Login Success');
+    }else{
+      console.log('Login fail');
+    }
+  }
 }
